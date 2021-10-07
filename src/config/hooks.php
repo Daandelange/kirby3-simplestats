00cc567d6e7948939c12c457f8a8adc3469d4830
Daan <idaany@gmail.com>
2021-10-07 15:26:31 +0200

    Fix route:after tracking on kirby 3.6

--- src/config/hooks.php
+++ src/config/hooks.php
@@ -8,14 +8,14 @@ use Throwable;
 
 return [
     ( option('daandelange.simplestats.tracking.onLoad', true) !== true )?[]:
-    'route:after' => function (/* $route, */ $path, $method, $result, $final) {
+    'route:after' => function ($path, $method, $result, $final) { // Available = ($route, $path, $method, $result, $final)
 
         // Call general track object
 
             if( $final === true && empty($result) === false && $method==='GET') { // Only log visits when the page object was found
                 //var_dump($result);
                 $page = $path;
-                if( is_a($result, 'Page') ) {
+                if( is_a($result, 'Kirby\Cms\Page') ) {
                     //var_dump( $result->id() );
                     $page = $result->id();
 
