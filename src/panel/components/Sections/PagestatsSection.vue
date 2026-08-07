c696661f5a02b5e1c72afffcc8044fd17a320d91
daan <idaany@gmail.com>
2026-08-07 12:10:34 +0200

    Fix (one)pagestats layout/sections

--- src/panel/components/Sections/PagestatsSection.vue
+++ src/panel/components/Sections/PagestatsSection.vue
@@ -1,16 +1,16 @@
 <template>
-  <k-grid variant="columns">
-    <!-- Loading message -->
-    <k-column v-if="isLoading" width="1/1">
-      <k-empty>
-        <k-icon type="loader"/>
-        <span>Fetching data...</span>
-      </k-empty>
-    </k-column>
+  <k-section :label="label">
+    <k-grid variant="columns">
+      <!-- Loading message -->
+      <k-column v-if="isLoading" width="1/1">
+        <k-empty>
+          <k-icon type="loader"/>
+          <span>Fetching data...</span>
+        </k-empty>
+      </k-column>
 
-    <!-- Total Visits -->
-    <k-column v-if="showTotals && !isLoading" width="1/1">
-      <k-section :label="label">
+      <!-- Total Visits -->
+      <k-column v-if="showTotals && !isLoading" width="1/1">
         <div class="k-stats">
           <k-stat
             :label="$t('simplestats.visits.visitsovertime')"
@@ -28,41 +28,41 @@
             link="simplestats"
           />
         </div>
-      </k-section>
-    </k-column>
+      </k-column>
 
-    <!-- Visits Over Time -->
-    <k-column v-if="showTimeline && !isLoading" width="1/1">
-      <k-simplestats-chart
-        type="Line"
-        download="PageVisitsOverTime.png"
-        :label="$t('simplestats.visits.visitsovertime')"
-        :chart-data="languagesOverTime"
-        :chart-labels="chartPeriodLabels"
-        :stacked="languagesAreEnabled"
-        :auto-colorize="true"
-        :x-time-axis="true"
-        :y-visits-axis="true"
-        :height="chartHeight('timeline')"
-      />
-    </k-column>
+      <!-- Visits Over Time -->
+      <k-column v-if="showTimeline && !isLoading" width="1/1">
+        <k-simplestats-chart
+          type="Line"
+          download="PageVisitsOverTime.png"
+          :label="$t('simplestats.visits.visitsovertime')"
+          :chart-data="languagesOverTime"
+          :chart-labels="chartPeriodLabels"
+          :stacked="languagesAreEnabled"
+          :auto-colorize="true"
+          :x-time-axis="true"
+          :y-visits-axis="true"
+          :height="chartHeight('timeline')"
+        />
+      </k-column>
 
-    <!-- Visits Per Language -->
-    <k-column v-if="languagesAreEnabled && showLanguages && !isLoading" width="1/1">
-      <k-simplestats-chart
-        type="Pie"
-        :chart-data="languageTotalHits"
-        :chart-labels="chartLanguagesLabels"
-        :chart-options="chartOptions"
-        :auto-colorize="true"
-        :fill="true"
-        :show-legend="languagesAreEnabled"
-        :height="chartHeight('languages')"
-        download="PageGlobalLanguageVisits.png"
-        :label="$t('simplestats.visits.globallanguages')"
-      />
-    </k-column>
-  </k-grid>
+      <!-- Visits Per Language -->
+      <k-column v-if="languagesAreEnabled && showLanguages && !isLoading" width="1/1">
+        <k-simplestats-chart
+          type="Pie"
+          :chart-data="languageTotalHits"
+          :chart-labels="chartLanguagesLabels"
+          :chart-options="chartOptions"
+          :auto-colorize="true"
+          :fill="true"
+          :show-legend="languagesAreEnabled"
+          :height="chartHeight('languages')"
+          download="PageGlobalLanguageVisits.png"
+          :label="$t('simplestats.visits.globallanguages')"
+        />
+      </k-column>
+    </k-grid>
+  </k-section>
 </template>
 
 <script>
